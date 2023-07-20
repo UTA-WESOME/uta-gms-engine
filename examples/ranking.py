9df73b94c4173ab154c814357b6d2fca885e403d
anras5 <anras1filip@gmail.com>
2023-07-20 19:01:29 +0200

    Add working xmcda files

--- examples/ranking.py
+++ examples/ranking.py
@@ -1,1 +1,7 @@
 # This file contains simple example of how to use uta-gms-engine package to make a ranking of variants
+from xmcda.XMCDA import XMCDA
+
+x = XMCDA()
+x.load(r'../tests/files/value_functions.xml')
+
+print(x)
