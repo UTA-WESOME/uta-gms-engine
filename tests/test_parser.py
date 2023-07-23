aeec910c3aaeae63446ac9d9218fa2a65714b831
beisert_j <jakub.beisert@student.put.poznan.pl>
2023-07-23 17:13:53 +0200

    Revert "adjust tests"
    This reverts commit 674f07ee772aa62f0a8a31889cb6e41b3067994a.

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,29 +1,27 @@
 import pytest
-import numpy as np
 
-from src.utagms.parser import Parser
+from src.utagms.parser import parser
 
 from typing import List
 
 
 @pytest.fixture()
-def performance_table_array_dummy():
-    return np.array([[26.0, 40.0, 44.0],
-                     [2.0, 2.0, 68.0],
-                     [18.0, 17.0, 14.0],
-                     [35.0, 62.0, 25.0],
-                     [7.0, 55.0, 12.0],
-                     [25.0, 30.0, 12.0],
-                     [9.0, 62.0, 88.0],
-                     [0.0, 24.0, 73.0],
-                     [6.0, 15.0, 100.0],
-                     [16.0, 9.0, 0.0],
-                     [26.0, 17.0, 17.0],
-                     [62.0, 43.0, 0.0]])
+def alternatives_array_dummy():
+    return [[26.0, 40.0, 44.0],
+            [2.0, 2.0, 68.0],
+            [18.0, 17.0, 14.0],
+            [35.0, 62.0, 25.0],
+            [7.0, 55.0, 12.0],
+            [25.0, 30.0, 12.0],
+            [9.0, 62.0, 88.0],
+            [0.0, 24.0, 73.0],
+            [6.0, 15.0, 100.0],
+            [16.0, 9.0, 0.0],
+            [26.0, 17.0, 17.0],
+            [62.0, 43.0, 0.0]]
 
 
-def test_get_performance_table_array(performance_table_array_dummy):
-    parser: Parser = Parser()
-    performance_table_array: np.ndarray = parser.get_performance_table_array('performance_table.xml')
+def test_get_alternatives_array(alternatives_array_dummy):
+    alternatives: List = parser.get_alternatives_array('performance_table.xml')
 
-    assert np.array_equal(performance_table_array, performance_table_array_dummy)
+    assert alternatives == alternatives_array_dummy
