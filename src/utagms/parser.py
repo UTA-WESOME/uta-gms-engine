144df0b26d31a2291b14307221ff89b3abbbef25
beisert_j <jakub.beisert@student.put.poznan.pl>
2023-07-26 14:46:30 +0200

    rename methods

--- src/utagms/parser.py
+++ src/utagms/parser.py
@@ -19,8 +19,8 @@ class Parser:
         :return: List of alternatives ex. [[26.0, 40.0, 44.0], [2.0, 2.0, 68.0], [18.0, 17.0, 14.0], ...]
         """
         performance_table_list: List[List] = []
-        xmcda: XMCDA = self.__load_file(path)
-        criterias_list: List = self.__get_criteria(path)
+        xmcda: XMCDA = self._load_file(path)
+        criterias_list: List = self._get_criteria(path)
 
         for alternative in xmcda.alternatives:
             performance_list: List = []
@@ -30,7 +30,7 @@ class Parser:
 
         return performance_table_list
 
-    def __get_criteria(self, path: str):
+    def _get_criteria(self, path: str):
         """
         Private method responsible for getting list of criterias
 
@@ -39,7 +39,7 @@ class Parser:
         :return: List of criteria ex. ['g1', 'g2', 'g3']
         """
         criteria_list: List = []
-        xmcda: XMCDA = self.__load_file(path)
+        xmcda: XMCDA = self._load_file(path)
         criteria_xmcda: Criteria = xmcda.criteria
 
         for criteria in criteria_xmcda:
@@ -47,7 +47,7 @@ class Parser:
 
         return criteria_list
 
-    def __load_file(self, path: str) -> XMCDA:
+    def _load_file(self, path: str) -> XMCDA:
         """
         Private method responsible for loading XMCDA files from tests/files location.
         To be refined later when we will read files from different location
