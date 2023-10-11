11cff427b95c3518a8bd6cd8a89be8904805cc6d
beisert_j <jakub.beisert@student.put.poznan.pl>
2023-10-11 18:25:59 +0200

    new Parser

--- src/utagmsengine/parser.py
+++ src/utagmsengine/parser.py
@@ -5,11 +5,12 @@ import _io
 from typing import List, Dict
 
 from .utils.parser_utils import ParserUtils
+from .dataclasses import Criterion
 
 
 class Parser:
     @staticmethod
-    def get_performance_table_dict_csv(csvfile: _io.TextIOWrapper) -> Dict[str,Dict[str,float]]:
+    def get_performance_table_dict_csv(csvfile: _io.TextIOWrapper) -> Dict[str, Dict[str, float]]:
         """
         Method responsible for getting dict of performances from CSV file
 
@@ -17,20 +18,38 @@ class Parser:
 
         :return: Dictionary of performances
         """
-        performance_table_list: List[List[float]] = []
         csv_reader = csv.reader(csvfile, delimiter=';')
-        criteria_ids: List[str] = next(csv_reader)
-        alternatives: List[str] = next(csv_reader)
+        gains: List[str] = next(csv_reader)[1:]  # skip gains row
+        criteria_ids: List[str] = next(csv_reader)[1:]
+
+        performance_table_list: List[List[float]] = []
+        alternative_ids: List[str] = []
         for row in csv_reader:
-            performance_list: List[float] = [float(value) for value in row]
+            performance_list: List[float] = [float(value) for value in row[1:]]
+            alternative_id: str = [value for value in row[:1]][0]
+
             performance_table_list.append(performance_list)
+            alternative_ids.append(alternative_id)
 
         result = {}
-        for idx, alternative in enumerate(alternatives):
-            result[alternative] = {criteria_id: performance for criteria_id, performance in
-                                   zip(criteria_ids, performance_table_list[idx])}
+        for i in range(len(alternative_ids)):
+            result[alternative_ids[i]] = {criteria_ids[j]: performance_table_list[i][j] for j in range(len(criteria_ids))}
 
         return result
+
+    @staticmethod
+    def get_criterion_list_csv(csvfile: _io.TextIOWrapper) -> List[Criterion]:
+        csv_reader = csv.reader(csvfile, delimiter=';')
+
+        gains: List[str] = next(csv_reader)[1:]
+        criteria_ids: List[str] = next(csv_reader)[1:]
+
+        criteria_objects = []
+        for i in range(len(criteria_ids)):
+            gain = True if gains[i].lower() == 'gain' else False
+            criteria_objects.append(Criterion(criterion_id=criteria_ids[i], gain=gain))
+
+        return criteria_objects
 
     def get_performance_table_list_xml(self, path: str) -> List[List]:
         """
@@ -84,44 +103,6 @@ class Parser:
 
         for criteria in criteria_xmcda:
             criteria_list.append(criteria.id)
-
-        # Recognition of the type of criteria
-        type_of_criterion: List[int] = []
-        for i in range(len(criteria_list)):
-            if criteria_list[i][0] == 'g':
-                type_of_criterion.append(1)
-            else:
-                type_of_criterion.append(0)
-
-        return type_of_criterion
-
-    @staticmethod
-    def get_alternatives_id_list_csv(csvfile: _io.TextIOWrapper) -> List[str]:
-
-        """
-        Method responsible for getting list of alternatives ids
-
-        :param csvfile: python file object of csv file
-
-        :return: List of alternatives ex. ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L']
-        """
-        csv_reader = csv.reader(csvfile, delimiter=';')
-        next(csv_reader)  # Skip the header row
-        alternatives_id_list = next(csv_reader)  # Read the second row (names row)
-
-        return alternatives_id_list
-
-    @staticmethod
-    def get_criteria_csv(csvfile: _io.TextIOWrapper) -> List[int]:
-        """
-        Method responsible for getting list of criteria
-
-        :param csvfile: python file object of csv file
-
-        :return: List of criteria ex. ['g1', 'g2', 'g3']
-        """
-        csv_reader = csv.reader(csvfile, delimiter=';')
-        criteria_list = next(csv_reader)
 
         # Recognition of the type of criteria
         type_of_criterion: List[int] = []
