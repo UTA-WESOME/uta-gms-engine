38704d935ce312e0adc054a74cf8e9e9babf04a4
anras5 <anras1filip@gmail.com>
2023-11-12 18:43:33 +0100

    Fix test filepath

--- tests/test_parser_utils.py
+++ tests/test_parser_utils.py
@@ -1,11 +1,10 @@
-import pytest
 from xmcda.XMCDA import XMCDA
 from src.utagmsengine.utils.parser_utils import ParserUtils
 import _io
 
 
 def test_load_file():
-    with open('performance_table.xml', 'rb') as xml_file:
+    with open('tests/files/performance_table.xml', 'rb') as xml_file:
         xml_file_text = _io.TextIOWrapper(xml_file, encoding='utf-8')
         xmcda: XMCDA = ParserUtils.load_xmcda(xml_file_text)
 
